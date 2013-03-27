3e00f80fd74bc96c29bcd72d10f8a22d893c871b
Raymond Tay <raymondtay1974@gmail.com>
2013-03-27 17:11:44 +0800

    Changes

--- src/scala/parser_combinators/Parsers.scala
+++ src/scala/parser_combinators/Parsers.scala
@@ -1,4 +1,6 @@
+import scala.language.higherKinds
 
+import property_testing._
 
 // The syntax that refers to Parser[_] is a type constructor
 // similar to the concept of haskell's type and data constructors
@@ -26,8 +28,31 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     // so we return a ParserError if we find errors or the type of the input i.e. A
     def run[A](p : Parser[A])(input: String) : Either[ParseError,A]
 
+    def listOfN[A](n: Int, p: Parser[A]) : List[Parser[A]] 
+
+    // We can add other binary operations to 'Parsers'
+    // and have 'ParserOps' delegate 
     case class ParserOps[A](p: Parser[A]) {
         def |[B>:A](p2: Parser[B]) : Parser[B] = self.or(p,p2)
         def or[B>:A](p2: => Parser[B]) : Parser[B] = self.or(p,p2)
+        def many[A] = self.many(p)
+        def map[B](f: A => B ) = self.map(p)(f)
+    }
+
+    // Let's consider the parser that recognizes 0 or more repetitions of 
+    // the character 'a' and returns the number of characters it has seen.
+    // combining 'map' we can write something like map(many(char('aaaa')))(_.length)
+    def many[A](p: Parser[A]) : Parser[List[A]]
+
+    def map[A,B](a: Parser[A])(f: A => B) : Parser[B]
+
+    // the first law has emerged!
+    // map(p)(id) == p
+    // i.e. a parser when given an identity fn, will always produce the same parser
+   
+    object Laws { 
+        def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]) : Prop = forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+        def mapLaw[A](p : Parser[A])(in: Gen[String]): Prop = equal(p, p.map(a => a))(in)
     }
 }
