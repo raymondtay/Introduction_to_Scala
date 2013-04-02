07ad5caa54f9f859743fc3868d5079b3904bb347
Raymond Tay <raymondtay1974@gmail.com>
2013-04-02 14:09:50 +0800

    Mods to make simple testing work

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,8 @@ import sbt._
 import sbt.Keys._
 
 object IntroToScalaBuild extends Build {
+    import ScalaTest._
+
     lazy val demo = Project(
     id = "introduction-to-scala",
     base = file("."),
@@ -10,7 +12,11 @@ object IntroToScalaBuild extends Build {
         version := "0.1-SNAPSHOT",
         scalaVersion := "2.10.0",
         scalacOptions ++= Seq("-feature", "-deprecation", "-language:postfixOps","-language:higherKinds", "-language:implicitConversions"),
-        resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases"
+        resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+        libraryDependencies ++= testDeps
         ) )
 }
 
+object ScalaTest {
+    val testDeps = Seq("org.scalatest" % "scalatest_2.10" % "1.9.1" % "test")
+}
--- src/test/scala/computation/TestMonoid.scala
+++ src/test/scala/computation/TestMonoid.scala
@@ -1,7 +1,15 @@
+package computation
 
-object TestMonoids extends App {
-    val words = List("a", "b", "hello", "there")
-    import Monoid._
-    words.foldRight(stringMonoid.id)(stringMonoid.op)
-    words.foldLeft(stringMonoid.id)(stringMonoid.op)
+import org.scalatest.FunSuite
+
+class TestMonoids extends FunSuite {
+
+    test("List of strings should concatenate properly with String Monoids") {
+        val words = List("a", "b", "hello", "there")
+        import Monoid._
+        val r1 = words.foldRight(stringMonoid.id)(stringMonoid.op)
+        val r2 = words.foldLeft(stringMonoid.id)(stringMonoid.op)
+        assert(r1 === words.mkString)
+        assert(r2 === words.mkString)
+    }
 }
