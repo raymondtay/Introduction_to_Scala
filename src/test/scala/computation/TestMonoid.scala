124915f170a3bb20a53b0e90e6cbef5f4e30835e
Raymond Tay <raymondtay1974@gmail.com>
2013-04-03 09:37:44 +0800

    Added another simple test for string monoids

--- src/test/scala/computation/TestMonoid.scala
+++ src/test/scala/computation/TestMonoid.scala
@@ -12,4 +12,10 @@ class TestMonoids extends FunSuite {
         assert(r1 === words.mkString)
         assert(r2 === words.mkString)
     }
+
+    test("Another test for List of string should concatenate the same with string monoids") {
+        val words = List("a", "b", "hello", "there")
+        import Monoid._
+        assert(words.foldLeft(stringMonoid.id)(stringMonoid.op) == (("" + "a") + "b") + "hellothere")
+    }
 }
