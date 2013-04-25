aa5797a6df26f08ffb0623cc9645639c8492400c
Raymond Tay <raymondtay1974@gmail.com>
2013-04-25 16:56:42 +0800

    more comments

--- src/main/java/simple/UnderstandExtends_2.java
+++ src/main/java/simple/UnderstandExtends_2.java
@@ -8,6 +8,9 @@ class HasF2 {
     public void f() { System.out.println("HasF.f()"); }
 }
 
+// What happens is that the type parameter erases to its first-bound
+// and in this example T erases to HasF2; it's also possible to have
+// multiple bounds on the type parameter
 class Manipulator2<T extends HasF2> {
     private T obj;
     public Manipulator2(T x ) { obj = x; }
@@ -21,3 +24,15 @@ class Manipulation2 {
         m.manipulate();
     }
 }
+
+/////////////////////////////
+// Binary compatibility
+/////////////////////////////
+// To allay any potential confusion about erasure, you must clearly understand that it is not a
+// language feature. It is a compromise in the implementation of Java generics.
+// In an erasure based implementation, generic types are treated as second class types that cannot be used
+// in some important contexts. The generic types are present only during static type checking after 
+// which every generic type in the program is erased by replacing it with a non-generic upper bound.
+
+// The core motivation for erasure is that it allows generified clients to be used with non-generified
+// libraries and vice versa. 
