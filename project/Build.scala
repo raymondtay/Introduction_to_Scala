a5072aba5d846db0b262e1cb6f178f2283f0f691
Raymond Tay <raymondtay1974@gmail.com>
2013-05-16 16:09:25 +0800

    Added JOCL to build

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,7 @@ import sbt.Keys._
 
 object IntroToScalaBuild extends Build {
     import ScalaTest._
+    import OpenCL._
 
     lazy val demo = Project(
     id = "introduction-to-scala",
@@ -13,10 +14,15 @@ object IntroToScalaBuild extends Build {
         scalaVersion := "2.10.0",
         scalacOptions ++= Seq("-feature", "-deprecation", "-language:postfixOps","-language:higherKinds", "-language:implicitConversions"),
         resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-        libraryDependencies ++= testDeps
+        libraryDependencies ++= Seq(testDeps, jocl)
         ) )
 }
 
 object ScalaTest {
-    val testDeps = Seq("org.scalatest" % "scalatest_2.10" % "1.9.1" % "test")
+    val testDeps = "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
 }
+
+object OpenCL {
+    val jocl = "com.nativelibs4java" % "javacl" % "1.0.0-RC3"
+}
+
