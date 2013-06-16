e7a6c467c2360c158d328aaa3f5a37161a448939
Raymond Tay <raymondtay1974@gmail.com>
2013-06-16 11:00:53 -0400

    Some more stuff

---
+++ src/main/scala/new_scala210/ImplicitCls.scala
@@ -0,0 +1,2 @@
+package new_scala210
+
---
+++ src/main/scala/new_scala210/StringInterpolation.scala
@@ -0,0 +1,9 @@
+package new_scala210
+
+// There are two primary ways to interpolate scala strings
+// in 2.10 and they are the 's', 'f' and 'raw' interpolators
+
+object StringInterpolator {
+
+}
+
