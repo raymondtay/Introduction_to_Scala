2e9dfb9d47e2887e6c026b7547babd3cde52863d
Raymond Tay <raymondtay1974@gmail.com>
2014-01-12 13:18:15 +0800

    getting lazy in writing comments

--- src/main/scala/computation/Monad.scala
+++ src/main/scala/computation/Monad.scala
@@ -13,19 +13,40 @@ trait Monad[F[_]] extends Functor[F] {
 
     def traverse[A,B](la: List[A])(f: A ⇒ F[B]) : F[List[B]] = la.foldRight(unit(List[B]()))( (a, fbs) ⇒ map2(f(a), fbs)(_ :: _))
 
+    def as[A,B](a: F[A])(b: B): F[B] = map(a)(_ ⇒ b)
+
     def filterM[A](as: List[A])(pred: A ⇒ F[Boolean]): F[List[A]] =
         as match {
             case Nil ⇒ unit(Nil)
             case h :: t ⇒ flatMap(pred(h))( v ⇒ if (!v) filterM(t)(pred) else map(filterM(t)(pred))(h :: _ )) 
         }
+
+    def compose[A,B,C](f: A ⇒ F[B])(g: B ⇒ F[C]) : A ⇒ F[C]
+
 }
+
+case class Id[A](value : A) {
+    def map[B](f: A ⇒ B) : Id[B] = Id(f(value))
+    def flatMap[B](f: A ⇒ Id[B]) : Id[B] = f(value)
+}
+
 
 object Monad {
     val optionM = 
         new Monad[Option] {
+            def compose[A,B,C](f: A ⇒ Option[B])(g: B ⇒ Option[C]) : A ⇒ Option[C] =
+                a ⇒ f(a) map { case (b:B) ⇒ g(b).get }
             def unit[A](a: ⇒ A) : Option[A] = Some(a)
             def flatMap[A,B](oa: Option[A])(f: A ⇒ Option[B]) : Option[B] = 
                 map(oa){a ⇒ (f(a) : @unchecked) match {case Some(v) ⇒ v}} // What is None of `B`??
+        }
+
+    val idM = 
+        new Monad[Id] {
+            def compose[A,B,C](f: A ⇒ Id[B])(g: B ⇒ Id[C]) : A ⇒ Id[C] = 
+                a ⇒ map(f(a))(x ⇒ g(x).value)
+            def flatMap[A,B](ia: Id[A])(f: A ⇒ Id[B]) = ia.flatMap(f)
+            def unit[A](a: ⇒ A) : Id[A] = Id(a)
         }
 }
 
