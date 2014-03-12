97a77d4ce25122b4c3ced7c2406ff30bc6799be5
Raymond Tay <raymondtay1974@gmail.com>
2014-03-12 21:48:14 +0800

    Added specs2

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,9 @@ object IntroToScalaBuild extends Build {
         version := "0.1-SNAPSHOT",
         scalaVersion := "2.10.0",
         scalacOptions ++= Seq("-feature", "-deprecation", "-language:postfixOps","-language:higherKinds", "-language:implicitConversions"),
+        resolvers ++= Seq("snapshots", "releases").map(Resolver.sonatypeRepo),
         resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+        scalacOptions in Test ++= Seq("-Yrangepos"),
         libraryDependencies ++= testDeps,
         libraryDependencies ++= Seq(jocl)
         ) )
@@ -22,7 +24,8 @@ object TestingDeps {
     val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.0.M6" % "test"
     val junit4Interface = "com.novocode" % "junit-interface" % "0.10-M4" % "test"
     val junit4 = "junit" % "junit" % "4.11" % "test"
-    val testDeps = Seq(scalaTest, junit4, junit4Interface)
+    val specs2 = "org.specs2" %% "specs2" % "2.3.10" % "test"
+    val testDeps = Seq(specs2, scalaTest, junit4, junit4Interface)
 }
 
 object OpenCL {
