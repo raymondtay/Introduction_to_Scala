f9674c8bb018d9e71ef8206aced66bbfbcd143f3
Raymond Tay <raymondtay1974@gmail.com>
2014-05-27 23:23:13 +0800

    slightly more stuff

--- src/main/java/simple/BinarySearchTree.java
+++ src/main/java/simple/BinarySearchTree.java
@@ -18,6 +18,29 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
     public Value get(Key key) { return get(root, key); }
 
+    public Key min() { return min(root).key; }
+
+    private Node min(Node x) { 
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
+
     private Value get(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
