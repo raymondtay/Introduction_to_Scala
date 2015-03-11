11791924f53f3326eb0371c4e1e6d8e053b7ded2
Raymond Tay <raymondtay1974@gmail.com>
2015-03-11 13:35:27 +0800

    spotted 1 bug

--- src/main/scala/advanced/FindPaths.scala
+++ src/main/scala/advanced/FindPaths.scala
@@ -231,7 +231,11 @@ object Find {
         import java.io._
         loadSteepestDescent
         data.toSet.map{(t: (Int,Int,List[Position])) => optfn(t._1, pathSteepestDescent.get._1) == t._1 match {
-                        case true  => pathSteepestDescent.set(t)
+                        case true  => 
+                            optfn(t._2, pathSteepestDescent.get._2) == t._2 match {
+                                case true  => pathSteepestDescent.set(t)
+                                case false =>
+                            }
                         case false =>
                       }
                 }
