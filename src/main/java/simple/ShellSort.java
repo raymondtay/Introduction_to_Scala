3207f068be1905e2bc1d66c91eb9be8ae7bfe04c
Raymond Tay <raymondtay1974@gmail.com>
2015-03-16 23:11:13 +0800

    [sorting] shell sort

---
+++ src/main/java/simple/ShellSort.java
@@ -0,0 +1,26 @@
+public class ShellSort {
+     public static void sort(Comparable[] a) {  // Sort a[] into increasing order.
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) h = 3*h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+        while (h >= 1)
+        {  // h-sort the array.
+           for (int i = h; i < N; i++)
+           {  // Insert a[i] among a[i-h], a[i-2*h], a[i-3*h]... .
+              for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+                 exch(a, j, j-h);
+           }
+           h = h/3; 
+        }
+    }
+
+    public static boolean less(Comparable x, Comparable y) {
+        return x.compareTo(y) < 0;
+    }
+    public static void exch(Comparable[] a, int x, int y) {
+        Comparable temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+}
+
