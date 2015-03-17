184109d22c81716c78c855558f67d1da353d6b14
Raymond Tay <raymondtay1974@gmail.com>
2015-03-17 13:14:52 +0800

    [algorithms] discovering inversions

---
+++ src/main/java/algorithms/Inversions.java
@@ -0,0 +1,62 @@
+/*
+Inversions. Develop and implement a linearithmic algorithm for computing the number of inversions in a given array (the number of exchanges that would be performed by insertion sort for that array
+*/
+import java.util.*;
+
+class Pair<A,B> {
+    Pair(A a, B b) { this.left = a; this.right = b; }
+    private final A left;
+    private final B right;
+    public A getLeft() { return this.left; }
+    public B getRight() { return this.right; }
+}
+
+public class Inversions {
+
+    private static boolean isInversion(Comparable a, Comparable b) { 
+        return a.compareTo(b) > 0;
+    }
+
+    private static ArrayList<Pair<Integer,Integer>> data = null;
+
+    // split the array into 2 halves each time, 
+    // search 1 halve followed by the other halve
+    // break down till there 2 leaves and perform function
+    // isInversion(x, [x1,x2]) where [x1,x2] are the leaves 
+    public static void findAllInversions(Comparable[] a, int low, int high) {
+        if (low == high) return ;
+        int mid = a.length/2;
+       
+        findInversions(a, low, mid);
+        findInversions(a, mid+1, high);
+    }
+
+    private static void findInversions(Comparable[] a, int low, int high) {
+        System.out.println("Examining indices ("+low+","+high+")");
+        if ( high == low ) return;
+        if ((high-low) == 1) {
+            if (isInversion(a[low], a[high]))
+                data.add(new Pair<Integer,Integer>(high,low));
+            else data.add(new Pair<Integer,Integer>(-99,-99)); // sentinel value
+        } else {
+            int mid = low + (high-low)/2;
+            findInversions(a, low, mid);
+            findInversions(a, mid+1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        data = new ArrayList<Pair<Integer,Integer>>();
+        int size = 100;
+        int count = size; 
+        Comparable[] a = new Comparable[count]; 
+        for(int i = 0; i < size; ++i, --count) 
+            a[i] = count;
+        findAllInversions(a, 0, a.length-1);
+        for(Pair<Integer,Integer> p : data)  {
+            if (p.getLeft() != -99) System.out.println("("+p.getLeft() +"," + p.getRight()+")");
+        }
+    }
+
+}
+
