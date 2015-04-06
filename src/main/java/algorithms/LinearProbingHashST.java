84d5da580b47d4e9a55e154a11124dc020d06fac
Raymond Tay <raymondtay1974@gmail.com>
2015-04-06 22:30:31 +0800

    [algorithms] linear-probing hash has delete and resize methods

--- src/main/java/algorithms/LinearProbingHashST.java
+++ src/main/java/algorithms/LinearProbingHashST.java
@@ -11,7 +11,40 @@ public class LinearProbingHashST<Key, Value> {
 
   private int hash(Key key) { return (key.hashCode() & 0x7fffffff) % M; }
 
-  private void resize() {}
+  private void resize(int cap) {
+    LinearProbingHashST<Key,Value> t;
+    t = new LinearProbingHashST<Key,Value>(cap);
+    for(int i = 0; i < M; ++i) if (keys[i] != null) t.put(keys[i], vals[i]);
+    keys = t.keys;
+    vals = t.vals;
+    M    = t.M;
+  }
+
+  private boolean contains(Key key) { 
+    boolean found = false;
+    for(int i = 0; i < M; ++i) 
+      if (key.equals(keys[i])) { found = true; break; }
+    return found;
+  }
+
+  public void delete(Key key) { 
+    if (!contains(key)) return;
+    int i = hash(key);
+    while(!key.equals(keys[i])) i = (i + 1) % M;
+
+    keys[i] = null;
+    vals[i] = null;
+    i = (i + 1) % M;
+    while(keys[i] != null) {
+      Key keyToRedo = keys[i];
+      Value valueToRedo = vals[i];
+      N--;
+      put(keyToRedo, valueToRedo);
+      i = (i + 1) % M;
+    }
+    N--;
+    if( N > 0 && N == M/8) resize(M/2);
+  }
 
   public void put(Key key, Value val) {
     if (N >= M/2) resize(2*M); // resize iff we are two times bigger
