6848dd36012237962ea781ee0d0f8132805de677
Raymond Tay <raymondtay1974@gmail.com>
2015-04-06 16:46:56 +0800

    [algorithms] separate-chaining impl of hash table

--- src/main/java/algorithms/SeparateChainingHashST.java
+++ src/main/java/algorithms/SeparateChainingHashST.java
@@ -1,3 +1,11 @@
+/* 
+  In a separate chaining has table with M lists and N keys, 
+  the probability that the number of keys in a list is within a small
+  constant factor of N/M is extremely close to 1.
+  But this is dependent on the fact that hash functions 
+  we use uniformly and independently distributes keys among 
+  the integer values between 0 and M - 1.
+*/
 public class SeparateChainingHashST<Key, Value> {
   private int N;
   private int M;
