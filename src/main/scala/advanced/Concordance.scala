2f5f82089ceb21f2f1e4efcf4b9f6d675078cedd
Raymond Tay <raymondtay1974@gmail.com>
2015-04-18 12:21:58 +0800

    [algorithm] inverted-index and concordance

---
+++ src/main/scala/advanced/Concordance.scala
@@ -0,0 +1,78 @@
+import scala.collection.immutable._
+import scala.io._
+import scala.util._
+import Source._
+import scala.math.Ordering._
+import Concordance.{Line,Idx}
+case class Position[Line,Idx](l: Line, idx: Idx) {
+  override def toString = s"line:$l, index: $idx"
+}
+
+object Concordance extends App {
+  type Line = Int
+  type Idx  = Int
+  private def addTm[A,B](tm: TreeMap[A,Queue[B]], key: A, value : B) = {
+    val curr_q : Queue[B] = 
+        Try(tm(key)) match {
+            case Success(q) => q
+            case Failure(_) => Queue()
+        }
+    tm + (key -> (Queue(curr_q:_*) :+ value))
+  }
+
+
+  override def main(args : Array[String]) : Unit = { 
+    var st = TreeMap[String,Queue[String]]()
+    var ts = TreeMap[String,Queue[String]]()
+    var ps = TreeMap[String,Queue[Position[Line, Idx]]]()
+    val filename = args(0)
+    val sep = args(1)
+    println(s"Analyzing $filename where each data is separate by $sep")
+    var currLine = 0
+    var currIdx  = 0
+    fromFile(filename).getLines.foreach{
+        line => 
+            currLine += 1 
+            val xs = line.split(sep) // array of strings or empty array
+            val key = xs.head
+            println(s"Analyzing data for $key")
+            currIdx = 0
+            for { 
+              w <- xs.tail 
+            } {
+              st.contains(key) match {
+                case false => 
+                    st = st + (key -> Queue.empty[String])
+                    ps = ps + (key -> Queue(Position(currLine, currIdx)))
+                case true  => 
+                    ts.contains(w) match {
+                        case false => 
+                            ts = ts + (w -> Queue.empty[String])
+                            ps = ps + (w -> Queue.empty[Position[Line,Idx]])
+                        case true  => 
+                    }
+
+              }
+              st = addTm(st, key, w)
+              ts = addTm(ts, w, key)
+              ps = addTm(ps, w, Position(currLine, currIdx))
+              currIdx += 1
+            }
+    }
+    println("Analysis done.")
+    stdin.getLines.foreach {
+      query => 
+        println(s"query is $query..")
+        st.contains(query) && ps.contains(query) match {
+          case true  => st(query).map(x => println("   " + x + " at position(s): " + ps(x)))
+          case false => 
+            ts.contains(query) match {
+              case true  => ts(query).map(x => println("   " + x + " at position(s): " + ps(x)))
+              case false => println("Didn't find anything.Try again or hit ctrl-c to quit")
+            }
+        }
+    }    
+  }
+}
+
+
